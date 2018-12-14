fff2b6d541e5a5c06a6cb693c1034ddb9b1b91ad
Dmitro Ovsiichuk <d.ovsiichuk@digitalscreens.com.ua>
2018-12-14 18:22:59 +0200

    fix

---
+++ src/OrderBundle/Controller/OrderBoardController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace OrderBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Cache;
+
+/**
+ * Class OrderBoardController
+ */
+class OrderBoardController extends Controller
+{
+    /**
+     * @param Request $request
+     *
+     * @return Response
+     *
+     * @Cache(maxage=60, public=true)
+     */
+    public function listAction(Request $request)
+    {
+        return $this->render('OrderBundle::list_genre.html.twig');
+    }
+}
